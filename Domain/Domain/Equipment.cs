dc12a64e98ff70ee6116e1d2d55806941b66e4b4
DanielF4918 <145735294+DanielF4918@users.noreply.github.com>
2025-03-25 16:56:03 -0600

    commit de sofia
    Se sube desde mi cuenta debido a errores desconocidos por parte del git de sofia, creacion de equipment en el backent(equipment service, Equipment DTO, controller)

--- Domain/Domain/Equipment.cs
+++ Domain/Domain/Equipment.cs
@@ -6,14 +6,10 @@ namespace Domain.Domain;
 public partial class Equipment
 {
     public int IdEquipment { get; set; }
-
     public string EquipmentName { get; set; } = null!;
-
     public string Description { get; set; } = null!;
-
     public string Category { get; set; } = null!;
-
     public decimal DailyRate { get; set; }
-
     public virtual ICollection<RentalDetail> RentalDetails { get; set; } = new List<RentalDetail>();
+
 }
