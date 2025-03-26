2e682f2c8775895eeff34c3f9700b6258d2fd23e
DanielF4918 <145735294+DanielF4918@users.noreply.github.com>
2025-03-25 18:00:57 -0600

    añadir rental dto

---
+++ BackEnd/DTO/RentalDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace BackEnd.DTO
+{
+    public class RentalDTO
+    {
+        public int IdRental { get; set; }
+        public int IdEvent { get; set; }
+        public DateTime RentalDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public decimal TotalCost { get; set; }
+    }
+}
+
